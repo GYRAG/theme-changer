df0161b36a50c77ce54adc959659626c9c473f6c
GYRAG <kinkladzegary@gmail.com>
2023-07-03 22:23:46 +0400

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,37 @@
+import React, { useState, useEffect } from 'react';
+import { useLocalStorage } from './Hooks/LocalStorage';
+import { useWindowSize } from './Hooks/WindowSize';
+import './App.css';
+
+function App() {
+  const [theme, setTheme] = useLocalStorage('theme', 'light');
+  const windowSize = useWindowSize();
+  const isDesktop = windowSize.width > 768; // Adjust the breakpoint as per your needs
+
+  useEffect(() => {
+    document.body.classList.remove('light', 'dark');
+    document.body.classList.add(theme);
+  }, [theme]);
+
+  const handleToggleTheme = () => {
+    setTheme(theme === 'light' ? 'dark' : 'light');
+  };
+
+  return (
+    <div className="App">
+      <header>
+        <h1>My Dark Mode App</h1>
+        {isDesktop && (
+          <button onClick={handleToggleTheme}>
+            Toggle Theme
+          </button>
+        )}
+      </header>
+      <main>
+        <p>here should be content.</p>
+      </main>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/css.jsx
@@ -0,0 +1,6 @@
+const darkThemeStyles = {
+    backgroundColor: '#000',
+    color: '#fff',
+    // Add more styles for the dark theme as needed
+  };
+  
